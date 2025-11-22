fd35e5711bc093f5f57ea561919b2a5de2e90868
Legolas <The-Legolas>
2025-11-22 23:54:56 +0100

    Did quite a few things today such as creating a render_attack_text function to display comabt text/logs, expanded the warrior subclass making it a bit more efficent and cleare in intent, created most of the logic for the enemy subclass and making it expandeble although it's yet to be finished in total I've spent around 2,5 today and 1,5 hours yesterday making it a total of 4 hours of coding doing this update and the last

--- Objects_code/Character_class.py
+++ Objects_code/Character_class.py
@@ -1,4 +1,5 @@
 import random
+from enum import Enum
 
 class Character():
     def __init__(self, name: str, hp: int, damage: int, defence: int, equipment: dict[str, any], level: int | None = None):
@@ -9,7 +10,7 @@ class Character():
         self.equipment = equipment
         self.level = level
 
-    def take_damage(self, damage) -> None:
+    def take_damage(self, damage: int) -> None:
         self.hp -= damage
     
     def is_alive(self) -> bool:
@@ -48,7 +49,7 @@ class Character():
 
         if random.random() >= 0.95:
             temp_damage *= 2
-            outcome_table["critical hit"] = True
+            outcome_table["critical_hit"] = True
         
         if temp_damage > other.defence:
             damage_dealt = temp_damage - other.defence
@@ -63,29 +64,151 @@ class Character():
         
         return outcome_table
     
+    def level_up(self) -> None:
+        pass
+
+    def render_attack_text(outcome: dict) -> str:
+
+        text_block = ""
+
+        text_block += "ATTACK RESULT:"
+        text_block += outcome["attacker"] + " attacks " + outcome["target"] + "\n"
+
+        if outcome["critical_hit"] is True:
+            text_block += "critical hit\n"
+
+        if outcome["blocked"] is True:
+            text_block += "The attack was blocked.\n"
+        else:
+            text_block += "Damage dealt: " + outcome["damage"] + "\n"
+
+        if outcome["warrior_rage"] == True:
+            text_block += "Warrior enters RAGE and deals extra damage!\n"
+
+        if outcome["died"] == True:
+            text_block += outcome["target"] + " has fallen.\n"
+
+        return text_block
+
+    
     def __str__(self):
-        return f"name:{self.name}, hp:{self.hp}, damage:{self.damage}, level:{self.level}"
+        return f"name:{self.name}, hp:{self.hp}, damage:{self.damage}, level:{self.level}, defence: {self.defence}, equipment: {self.equipment}"
     
 
 class Warrior(Character):
-    def __init__(self, name, hp, damage, defence, equipment, level = None):
+    def __init__(self, name: str, hp: int, damage: int, defence: int, equipment: dict[str, any], level: int | None = None):
         super().__init__(name, hp, damage, defence, equipment, level)
-
-
-
-
-class Enemy():
-    def __init__(self, name: str, hp: int, damage: int):
-        self.name = name
-        self.hp = hp
-        self.damage = damage
-
-    def list() -> list:
-        list_of_enemies = []
+        self.defence *= 1.2
+        self.hp *= 1.2
+        self.damage *= 0.9
+        self.max_hp = self.hp
     
-        list_of_enemies.append(Enemy("bat", 10, 2))
-        list_of_enemies.append(Enemy("skeleton", 5, 10))
-        list_of_enemies.append(Enemy("ogre", 100, 30))
+    def take_damage(self, damage: int):
+        reduced_damage = damage * 0.9
+        return super().take_damage(reduced_damage)
+    
+    def attack(self, other: 'Character'):
+        outcome = super().attack(other)
+        if outcome["blocked"] == True:
+            return outcome
 
-        return list_of_enemies
+        if self.hp <= (self.max_hp * 0.40) and random.random() >= 0.30: #extra damage eg. rage which will be implemented later
+            outcome["warrior_rage"] = True
+
+            extra_damage = int(outcome["damage"] * 0.5)
+            
+            other.take_damage(extra_damage)
+            outcome["damage"] += extra_damage
+
+        return outcome
+
+    
+
+    def level_up(self): #this method should be called exp_up but I don't want to implement it before I know how hard to make the enemies
+        if self.level == 10:
+            return "Cannot increase to more than 10" # should be reworked when xp have been implemented so the xp just go up and level doesn't change
+        temp_level = self.level
+                
+        #add xp gain later for now I will just increase level by 1 when this method is called
+        self.level += 1
+
+        if self.level > temp_level:
+            if self.level <= 5:
+                self.defence *= 1.3
+                self.hp *= 1.3
+                self.damage *= 1.2
+            
+            elif self.level <= 8:
+                self.defence *= 1.4
+                self.hp *= 1.5
+                self.damage *= 1.3
+            
+            elif self.level == 9:
+                self.defence *= 1.6
+                self.hp *= 1.7
+                self.damage *= 1.4
+            
+            elif self.level == 10:
+                self.defence *= 1.8
+                self.hp *= 1.9
+                self.damage *= 1.6
+
+
+
+
+class Enemy_Rarity(Enum):
+    COMMON = "common"
+    UNCOMMON = "uncommon"
+    RARE = "rare"
+    ELITE = "elite"
+    MINI_BOSS = "mini_boss"
+    BOSS = "boss"
+
+class Enemy_type(Enum): #only 1 for don't know how many I'll add
+    UNDEAD = "undead"
+
+class Behavior_Tag(Enum):
+    NORMAL = "normal" # default state
+    AGGRESSIVE = "aggressive"
+    COWARDLY = "cowardly"
+    RANGED = "ranged"
+    SLOW = "slow"
+
+class Enemy(Character):
+    def __init__(self, name: str, hp: int, damage: int, 
+                 defence: int, rarity: Enemy_Rarity, 
+                 type: Enemy_type, loot_table: list[dict[str, any]],
+                 behavior_tag: Behavior_Tag | None = None):
+        super().__init__(name, hp, damage, defence)
+        
+        self.type = type
+        self.rarity = rarity
+
+        self.level_scaling_factor = None # TBA
+
+        self.loot_table = loot_table
+
+        self.behavior_tag = behavior_tag if behavior_tag else self.behavior_tag = Behavior_Tag.NORMAL
+    
+    def attack(self, other: 'Character') -> dict: # Will be modified later on but for now will act as normal
+        return super().attack(other)
+    
+    def take_damage(self, damage: int) -> None: # Will be modified later on but for now will act as normal
+        return super().take_damage(damage)
+    
+    def scale_stats(self, day_counter: int, depth) -> None:
+        day_counter_scaling = 1 + (day_counter / 100)
+        depth_scaling = 1 + (depth / 50)
+
+        self.hp = self.hp * day_counter_scaling
+        self.defence = self.defence * day_counter_scaling
+
+        self.damage = self.damage * depth_scaling
+        
+
+
+
+    
+
+    
 
